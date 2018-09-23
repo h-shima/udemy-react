9106aaddb2a0cc79505c3592e9782ea121ff13c1
hirokioshima <geet1876@gmail.com>
2018-09-23 14:13:22 +0900

    Add GeocodeResult

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +18,12 @@ class App extends Component {
     return (
       <div>
         <h1>緯度経度検索</h1>
-        <SearchForm onSubmit={(place) => this.handlePlaceSubmit(place)} />
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+         />
       </div>
     );
   }
